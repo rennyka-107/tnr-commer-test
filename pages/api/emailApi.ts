9a2340525247a773ffcdba417aca61c12263caaf
Nguyen Duc Long <longnd@sphinxjsc.com>
2022-08-23 14:44:00 +0700

    cm code

--- pages/api/emailApi.ts
+++ pages/api/emailApi.ts
@@ -2,8 +2,9 @@ import { PTGType, ProductsType, ParamsProducts } from "interface/product";
 import { CommonResponse, ResponseTypeAPI } from "type/common";
 import HttpClient from "utils/HttpClient";
 
-export const postEmailRegister = async(email: any) => {
-	return HttpClient.post<any, CommonResponse>(`/api/project/information/subscribe/email`,{
-		email: email
-	})
-}
+export const postEmailRegister = async (email: any) => {
+  return HttpClient.post<any, CommonResponse>(
+    `/api/project/information/subscribe/email`,
+    email
+  );
+};
